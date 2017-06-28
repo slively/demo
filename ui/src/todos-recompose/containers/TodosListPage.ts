03bb1be9e21af56f4a9382d317c2be7b5479ac14
slively <scott.lively@gmail.com>
2017-06-27 20:58:20 -0700

    correctly handle empty search, fix adding multiple todos

--- ui/src/todos-recompose/containers/TodosListPage.ts
+++ ui/src/todos-recompose/containers/TodosListPage.ts
@@ -22,34 +22,35 @@ interface UrlState {
 
 interface AddedTodosState {
 	addedTodos: TodoEntity[];
-	addTodoToState: (todo: TodoEntity) => void;
+	setAddedTodos: (addedTodos: TodoEntity[]) => void;
 }
 
 type TodosListDataProps = TodosFindResponse & UrlState & AddedTodosState;
 
+const selectSearchText = props => props.searchText || '';
 const selectFilteredTodos = createSelector<TodosListDataProps, TodoEntity[], string, TodoEntity[], TodoEntity[]>(
 	props => props.promiseData || [],
-	props => props.searchText || '',
+	selectSearchText,
 	props => props.addedTodos,
 	(todos, searchText, addedTodos) => ((searchText.length > 0) ? todos.filter(todo => todo.text.includes(searchText)) : todos).concat(addedTodos)
 );
 
 const selectSearchProps = (props: TodosListDataProps): TodosSearchProps => ({
-	searchText: props.searchText,
+	searchText: selectSearchText(props),
 	setSearchText: searchText => {
 		const state = Routing.router.getState() as RouteState;
 
-		Routing.router.navigate(state.name, {searchText}, {replace: true, reload: true});
+		Routing.router.navigate(state.name, {searchText: searchText.length ? searchText : undefined}, {replace: true, reload: true});
 	}
 });
 
 export const TodosListPage = compose<TodosListProps, {}>(
 	withPromise<TodoEntity[]>(() => TodosService.find()),
-	withState<AddedTodosState>('addedTodos', 'addTodoToState', []),
+	withState<AddedTodosState>('addedTodos', 'setAddedTodos', []),
 	withObservable<UrlState>(Routing.route$.map((state: RouteState) => state.params as UrlState)),
 	mapProps<TodosListProps, TodosListDataProps>(
 		props => ({
-			addTodo: (todo: TodoEntity) => TodosService.create(todo).then(addedTodo => props.addTodoToState(addedTodo)),
+			addTodo: (todo: TodoEntity) => TodosService.create(todo).then((addedTodo: TodoEntity) => props.setAddedTodos(props.addedTodos.concat(addedTodo))),
 			todos: selectFilteredTodos(props),
 			search: selectSearchProps(props)
 		})
